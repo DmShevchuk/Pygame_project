919c427c44b727c35591214b72c656857c049fd1
Dmitrii-Shevchuk <dmitriishevchuk2003@yandex.ru>
2019-12-25 22:41:49 +0300

    Добавлен экран загрузки, пересечение игрока с платформой, стрельба.

--- Main.py
+++ Main.py
@@ -1,12 +1,13 @@
 import pygame
-import os
-
+import random
+import os, sys
 
 pygame.init()
 SIZE = [600, 400]
 screen = pygame.display.set_mode(SIZE)
 BLACK = pygame.Color('black')
 screen.fill(BLACK)
+
 
 def load_image(name, color_key=None):
     fullname = os.path.join('data', name)
@@ -25,10 +26,77 @@ def load_image(name, color_key=None):
     return image
 
 
+def start_screen():
+    fon = pygame.transform.scale(load_image('fon2.jpg'), (SIZE[0], SIZE[1]))
+    screen.blit(fon, (0, 0))
+    title = ['T', 'h', 'e', 'g', 'a', 'm', 'e']
+    shoot = True
+    bullet_update = True
+    FPS = 60
+    count = 0
+    index = 0
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        hero_group.draw(screen)
+        bullets_group.draw(screen)
+        birds_group.draw(screen)
+        if hero.rect.x < SIZE[0] // 3:
+            hero.update(2, 0, check_collide=False)
+        elif shoot:
+            Bullet(hero.rect.x, hero.rect.y)
+            shoot = False
+        if bullet_update:
+            for elem in bullets_group:
+                elem.update(x=10, y=-10)
+                if elem.rect.y < SIZE[1] // 3:
+                    elem.kill()
+                    fon = pygame.transform.scale(load_image('fon3.jpg'), (SIZE[0], SIZE[1]))
+                    screen.blit(fon, (0, 0))
+                    Bird(load_image('bird1.png'), 5, 1, 500, SIZE[1] // 4)
+                    FPS = 30
+                    bullet_update = False
+        else:
+            for bird in birds_group:
+                bird.update()
+            if count % 10 == 0:
+                create_particles((bird.rect.x + 50, bird.rect.y))
+                index += 1
+            count += 15
+            text_coord = bird.rect.x + 50
+            font = pygame.font.Font(None, 50)
+            for line in title[:index]:
+                string_rendered = font.render(line, 1, pygame.Color('blue'))
+                intro_rect = string_rendered.get_rect()
+                text_coord += 10
+                intro_rect.left = text_coord
+                intro_rect.y = bird.rect.y
+                text_coord += intro_rect.height
+                screen.blit(string_rendered, intro_rect)
+        pygame.display.flip()
+        screen.blit(fon, (0, 0))
+        stars_group.update()
+        stars_group.draw(screen)
+        pygame.time.Clock().tick(FPS)
+        clock.tick(FPS)
+
+
+def create_particles(position):
+    particle_count = 15
+    numbers = range(-5, 6)
+    for _ in range(particle_count):
+        Particle(position, random.choice(numbers), random.choice(numbers))
 
 
 hero_group = pygame.sprite.Group()
 levels_group = pygame.sprite.Group()
+bullets_group = pygame.sprite.Group()
+birds_group = pygame.sprite.Group()
+stars_group = pygame.sprite.Group()
+
 
 class Hero(pygame.sprite.Sprite):
     def __init__(self, sheet, columns, rows, x, y):
@@ -38,6 +106,7 @@ class Hero(pygame.sprite.Sprite):
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
         self.rect = self.rect.move(x, y)
+        self.health = 5
 
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
@@ -48,16 +117,16 @@ class Hero(pygame.sprite.Sprite):
                 self.frames.append(sheet.subsurface(pygame.Rect(
                     frame_location, self.rect.size)))
 
-    def update(self, x, y):
+    def update(self, x, y, check_collide=True):
         self.rect.x += x
         self.rect.y += y
-        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-        #!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        if y < 0:
-            self.image = pygame.transform.flip(self.frames[self.cur_frame], 1, 0)
-        else:
+        if not pygame.sprite.collide_mask(self, level) or not check_collide:
+            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
             self.image = self.frames[self.cur_frame]
-        self.mask = pygame.mask.from_surface(self.image)
+            self.mask = pygame.mask.from_surface(self.image)
+        else:
+            self.rect.x -= x
+            self.rect.y -= y
 
 
 class Level(pygame.sprite.Sprite):
@@ -71,24 +140,97 @@ class Level(pygame.sprite.Sprite):
         self.rect.bottom = SIZE[1]
 
 
+class Bullet(pygame.sprite.Sprite):
+    screen_rect = (0, 0, SIZE[0], SIZE[1])
+
+    def __init__(self, x, y):
+        super().__init__(bullets_group)
+        self.image = load_image('bullet.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = x + (hero.rect.width // 4) * 3
+        self.rect.y = y + hero.rect.height // 3
+
+    def update(self, x=15, y=0):
+        self.rect.x += x
+        self.rect.y += y
+        if not self.rect.colliderect(Bullet.screen_rect):
+            self.kill()
+
+
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(birds_group)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+        self.health = 5
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.rect.x -= 5
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+        self.mask = pygame.mask.from_surface(self.image)
+        if not self.rect.colliderect((50, 0, SIZE[0], SIZE[1])):
+            self.kill()
+
+
+class Particle(pygame.sprite.Sprite):
+    fire = [load_image("star.png")]
+    for scale in (5, 10, 20):
+        fire.append(pygame.transform.scale(fire[0], (scale, scale)))
+
+    def __init__(self, pos, dx, dy):
+        super().__init__(stars_group)
+        self.image = random.choice(self.fire)
+        self.rect = self.image.get_rect()
+        self.velocity = [dx, dy]
+        self.rect.x, self.rect.y = pos
+        self.gravity = 0.25
+
+    def update(self):
+        self.velocity[1] += self.gravity
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        if not self.rect.colliderect((0, 0, SIZE[0], SIZE[1])):
+            self.kill()
+
+
 level = Level('first')
-start_y = level.rect.y - level.rect.height - 10
-hero = Hero(load_image("hero.png"), 6, 1, 30, start_y)
+start_y = level.rect.y - level.rect.height - 15
+hero = Hero(load_image("hero.png"), 6, 1, 30, 300)
 
 clock = pygame.time.Clock()
 FPS = 50
 running = True
+start_screen_show = True
 x, y = 0, 0
 
 dy = 0
 
 while running:
+    if start_screen_show:
+        start_screen()
+        start_screen_show = False
+        hero_group = pygame.sprite.Group()
+        hero = Hero(load_image("hero.png"), 6, 1, 30, start_y)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                    x, y = 5, 0
+                x, y = 5, 0
             if event.key == pygame.K_LEFT:
                 if dy == 5:
                     x, y = -3, 0
@@ -96,13 +238,15 @@ while running:
                     x, y = -5, 0
             if event.key == pygame.K_UP:
                 if dy != 5:
-                    hero.update(0, -100)
+                    hero.update(0, -95)
                     dy = 5
+            if event.key == pygame.K_SPACE:
+                Bullet(hero.rect.x, hero.rect.y)
         if event.type == pygame.KEYUP:
-            if event.key in(pygame.K_RIGHT, pygame.K_LEFT, pygame.K_UP, pygame.K_DOWN):
+            if event.key in (pygame.K_RIGHT, pygame.K_LEFT, pygame.K_UP, pygame.K_DOWN):
                 x, y = 0, 0
 
-    if x != 0 and y ==0:
+    if x != 0 and y == 0:
         hero.update(x, y)
     if dy != 0:
         if start_y > hero.rect.y:
@@ -112,6 +256,9 @@ while running:
     screen.fill(BLACK)
     hero_group.draw(screen)
     levels_group.draw(screen)
+    bullets_group.draw(screen)
+
+    for bullet in bullets_group:
+        bullet.update()
     clock.tick(FPS)
     pygame.display.flip()
-
